45c081cde8b101b2330ec8101b1d79e687bfc76a
Alexander ShvetsÐ <box0259@gmail.com>
2022-08-23 15:17:27 +0400

    commit

---
+++ src/main/java/guiModGen.java
@@ -0,0 +1,48 @@
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+
+
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+import java.util.Objects;
+
+
+
+
+public class guiModGen extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass()
+                    .getResource("gui.fxml")));
+            primaryStage.setTitle("Генератор v1.0");
+            primaryStage.setScene(new Scene(root, 300, 430, Color.web("#3C3F41")));
+            //primaryStage.getIcons().add(new Image("picture/favicon.png"));
+            primaryStage.setResizable(false);
+            primaryStage.show();
+            // запускаем основной класс
+
+            primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+                @Override
+                public void handle(WindowEvent event) {
+                    Platform.exit();
+                    System.exit(0);
+                }
+            });
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
